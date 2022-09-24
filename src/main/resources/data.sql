2390af8869482d2dbf4b54a416b5c69a8c298120
nsrlhn <nsrlhn@gmail.com>
2022-09-25 02:17:16 +0300

    Example Data: Employee

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+--EMPLOYEE
+INSERT INTO EMPLOYEE (id, name, recruitment) VALUES (1, 'Ensar', '2000-01-01');
+INSERT INTO EMPLOYEE (id, name, recruitment) VALUES (2, 'Ilhan', '2020-01-01');
+INSERT INTO EMPLOYEE (id, name, recruitment) VALUES (3, 'Burak', '2019-01-01');
